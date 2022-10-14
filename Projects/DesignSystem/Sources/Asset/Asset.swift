a960c6f390b84b8dbc3a5be647e9cfe45eb0f346
Chanhee Jeong <chan420420@gmail.com>
2022-10-14 11:43:28 +0900

    Feat✨: Color 및 Asset Extension 추가

---
+++ Projects/DesignSystem/Sources/Asset/Asset.swift
@@ -0,0 +1,15 @@
+//
+//  Asset.swift
+//  DesignSystem
+//
+//  Created by Chanhee Jeong on 2022/10/14.
+//  Copyright © 2022 zesty. All rights reserved.
+//
+
+import Foundation
+
+public enum Asset: String {
+    case temp // FIXME: - 추후 삭제 요망
+//    case ic_test  => 나중에 이렇게 해주세요
+}
+
---
+++ Projects/DesignSystem/Sources/Color/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  DesignSystem
+//
+//  Created by Chanhee Jeong on 2022/10/14.
+//  Copyright © 2022 zesty. All rights reserved.
+//
+
+import Foundation
+
+public enum Pallete: String {
+    case grayC5
+
+    var hexString: String {
+        switch self {
+        case .grayC5:
+            return "#C5C5C5FF"
+        }
+    }
+}
