ed6ef679c8dddd306cc2ca9f5b890492be5ed041
Lia316 <kjk0825@postech.ac.kr>
2022-10-14 11:26:50 +0900

    Chore🧹: review - toDictionary fileprivate 접근 제어 추가

--- Projects/Network/Sources/EndPoint/Requestable.swift
+++ Projects/Network/Sources/EndPoint/Requestable.swift
@@ -65,7 +65,7 @@ extension Requestable {
 
 extension Encodable {
 
-    func toDictionary() throws -> [String: Any]? {
+    fileprivate func toDictionary() throws -> [String: Any]? {
         let data = try JSONEncoder().encode(self)
         let jsonData = try JSONSerialization.jsonObject(with: data)
         return jsonData as? [String: Any]
