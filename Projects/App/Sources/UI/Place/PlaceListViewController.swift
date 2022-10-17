0b6d248832def80914765cd3dcbb57a319b482ea
Lia316 <kjk0825@postech.ac.kr>
2022-10-17 10:55:52 +0900

    Setting⚙️: initial setting

---
+++ Projects/App/Sources/UI/Place/PlaceListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PlaceListViewController.swift
+//  App
+//
+//  Created by 리아 on 2022/10/17.
+//  Copyright © 2022 zesty. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+import DesignSystem
+
+final class PlaceListViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Function
+    
+}
+
+// MARK: - UI Function
+
+extension PlaceListViewController {
+    
+    private func configureUI() {
+        
+    }
+    
+    private func createLayout() {
+        
+    }
+    
+}
+
+// MARK: - Preview
+
+struct PlaceListViewControllerTemplatePreview: PreviewProvider {
+    
+    static var previews: some View {
+        PlaceListViewController().toPreview()
+    }
+
+}
