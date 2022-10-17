0f12ed1a4b06a395bcfc249d20c15a5fc4a36b5f
Goban <gobanest@gmail.com>
2022-10-17 17:49:35 +0900

    HOTFIX:ambulance:: 스크립트 잘 동작안되는거 수정

--- Scripts/set_template.sh
+++ Scripts/set_template.sh
@@ -1,1 +1,21 @@
-cp -r Templates/ZESTY_VC.xctemplate ~/Library/Developer/Xcode/Templates/File\ Templates/
+#!/usr/bin/env sh
+
+# Configuration
+XCODE_TEMPLATE_DIR=$HOME'/Library/Developer/Xcode/Templates/'
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+xcodeTemplate () {
+  echo "==> Copying Xcode file templates..."
+
+  if [ -d "$XCODE_TEMPLATE_DIR" ]; then
+    rm -R "$XCODE_TEMPLATE_DIR"
+  fi
+  mkdir -p "$XCODE_TEMPLATE_DIR"
+
+  cp -R $SCRIPT_DIR/*.xctemplate "$XCODE_TEMPLATE_DIR"
+}
+
+xcodeTemplate
+
+echo "==> ... success!"
+echo "==> select ZESTY Template!"
