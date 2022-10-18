6034b385188dd3007ea37663a403ff4669326c48
Lia316 <kjk0825@postech.ac.kr>
2022-10-18 10:38:32 +0900

    Feat✨: 임시 banner view 생성

---
+++ Projects/App/Sources/UI/Place/BannerView.swift
@@ -0,0 +1,66 @@
+//
+//  BannerView.swift
+//  App
+//
+//  Created by 리아 on 2022/10/17.
+//  Copyright © 2022 zesty. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+import SnapKit
+
+final class BannerCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setView() {
+        let bannerView = BannerView()
+        let controller = UIHostingController(rootView: bannerView)
+        addSubview(controller.view)
+        controller.view.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+}
+
+struct BannerCellPreview: PreviewProvider {
+    
+    static var previews: some View {
+        BannerCell().toPreview()
+    }
+
+}
+
+struct BannerView: View {
+
+    @State private var selected = 0
+    
+    var body: some View {
+        TabView(selection: $selected) {
+            ForEach(0..<4) { index in
+                Text("\(index)")
+                    .tag(index)
+                    .tabItem {
+                        Image(systemName: (selected == index ? "circle.fill" : "circle"))
+                            .environment(\.symbolVariants, .none)
+                    }
+            }
+        }
+        .onAppear {
+            UIPageControl.appearance().currentPageIndicatorTintColor = .black
+            UIPageControl.appearance().pageIndicatorTintColor = .black
+        }
+        .tabViewStyle(PageTabViewStyle())
+        
+    }
+    
+}
