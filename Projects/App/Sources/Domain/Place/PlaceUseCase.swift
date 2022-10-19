cc3e19b976f0aa408cc32d10edec18b328d289d0
Lia316 <kjk0825@postech.ac.kr>
2022-10-19 15:15:10 +0900

    Feat✨: fetchPlaceList UseCase 작성

---
+++ Projects/App/Sources/Domain/Place/PlaceUseCase.swift
@@ -0,0 +1,30 @@
+//
+//  PlaceUseCase.swift
+//  App
+//
+//  Created by 리아 on 2022/10/19.
+//  Copyright © 2022 zesty. All rights reserved.
+//
+
+import Combine
+import Foundation
+import Network
+
+final class PlaceListUseCase {
+    
+    private var cancelBag = Set<AnyCancellable>()
+    
+    func fetchPlaceList() {
+        print("ing...")
+        API.fetchPlaceList()
+            .sink { error in
+                print(error)
+            } receiveValue: { placeListDTOs in
+                print(placeListDTOs)
+                let placeList = placeListDTOs.map { Place(dto: $0) }
+                print(placeList)
+            }
+            .store(in: &cancelBag)
+    }
+    
+}
