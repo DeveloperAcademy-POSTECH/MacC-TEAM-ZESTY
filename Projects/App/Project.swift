3cceb8450cfed7b5f268edf3e5c31e68545c2100
Goban <gobanest@gmail.com>
2022-10-19 11:08:37 +0900

    Setting:gear:: Alamofire 삭제

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -14,14 +14,12 @@ let project = Project.makeModule(
     product: .app,
     packages: [
         .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.0.1")),
-        .remote(url: "https://github.com/Alamofire/Alamofire.git", requirement: .upToNextMajor(from: "5.6.1")),
         .remote(url: "https://github.com/onevcat/Kingfisher.git", requirement: .upToNextMajor(from: "7.0.0"))
     ],
     dependencies: [
         .Projcet.Network,
         .Projcet.DesignSystem,
         .package(product: "SnapKit"),
-        .package(product: "Alamofire"),
         .package(product: "Kingfisher")
     ],
     resources: ["Resources/**"],
