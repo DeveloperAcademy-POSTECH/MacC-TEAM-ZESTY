f4f00a1eb452a87413159b11ea875b3e6880cc62
Goban <gobanest@gmail.com>
2022-10-19 12:22:08 +0900

    Setting:gear:: Kakao SDK 추가

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -14,11 +14,13 @@ let project = Project.makeModule(
     product: .app,
     packages: [
         .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.0.1")),
-        .remote(url: "https://github.com/onevcat/Kingfisher.git", requirement: .upToNextMajor(from: "7.0.0"))
+        .remote(url: "https://github.com/onevcat/Kingfisher.git", requirement: .upToNextMajor(from: "7.0.0")),
+        .remote(url: "https://github.com/kakao/kakao-ios-sdk", requirement: .upToNextMajor(from: "2.11.3"))
     ],
     dependencies: [
         .Projcet.Network,
         .Projcet.DesignSystem,
+        .package(product: "KakaoSDK"),
         .package(product: "SnapKit"),
         .package(product: "Kingfisher")
     ],
