eb0c84d1dcc303141252f924f4443e77c04dc8c6
Nick(taehoKim) <xogh8025@gmail.com>
2022-10-21 10:17:05 +0900

    Chore:broom:: subtitle 예외 처리
    subtitle을 예외 처리 하였습니다
    - subtitle에 값이 들어오지 않을 경우 빈 문자열이 리턴됩니다.
    - 빈 문자열이 들어올 경우 subtitle의 layout이 잡히지 않도록 했습니다

--- Projects/DesignSystem/Sources/Component/MainTitleView.swift
+++ Projects/DesignSystem/Sources/Component/MainTitleView.swift
@@ -12,47 +12,76 @@ import SnapKit
 /// Main Title을 만들기 위한 UIView
 /// - title: 화면의 타이틀에 들어갈 문자열
 /// - subtitle: (옵셔널) 화면의 타이틀 밑에 들어갈 회색의 문자열
+///
+/// 높이는 최소 94 point로 정해져 있습니다.
+///
+/// top constraint와 너비만 지정해주면 됩니다
 public final class MainTitleView: UIView {
-    
     private let titleLabel = UILabel()
     private let subtitleLabel = UILabel()
+    private let titleStackView = UIStackView()
     
-    public init(title: String, subtitle: String? = nil) {
+    public init(title: String, subtitle: String? = "") {
         super.init(frame: .zero)
+        
         titleLabel.text = title
         subtitleLabel.text = subtitle
         
-        configureUI()
-        createLayout()
+        guard let subtitle = subtitle else {return}
+        
+        configureUI(subTitle: subtitle)
+        createLayout(subtitle: subtitle)
     }
     
-    required init?(coder: NSCoder) {
+    required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
 extension MainTitleView {
     
-    private func configureUI() {
+    private func configureUI(subTitle: String) {
+        titleStackView.spacing = 12
+        titleStackView.axis = .vertical
+        titleStackView.distribution = .fill
+        
         titleLabel.numberOfLines = 0
         titleLabel.font = .systemFont(ofSize: 26, weight: .bold)
         
-        subtitleLabel.textColor = .secondaryLabel
-        subtitleLabel.numberOfLines = 0
-        subtitleLabel.font = .systemFont(ofSize: 16, weight: .regular)
+        if !subTitle.isEmpty {
+            subtitleLabel.textColor = .secondaryLabel
+            subtitleLabel.numberOfLines = 0
+            subtitleLabel.font = .systemFont(ofSize: 16, weight: .regular)
+        }
     }
     
-    private func createLayout() {
-        addSubviews([titleLabel, subtitleLabel])
+    private func createLayout(subtitle: String) {
+        addSubview(titleStackView)
+        titleStackView.addArrangedSubview(titleLabel)
         
-        titleLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(30)
-            make.horizontalEdges.equalToSuperview().inset(20)
+        self.snp.makeConstraints { make in
+            make.height.greaterThanOrEqualTo(94)
         }
         
-        subtitleLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(12)
+        titleStackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(30)
             make.horizontalEdges.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
+            make.height.greaterThanOrEqualTo(31)
+        }
+        
+        if !subtitle.isEmpty {
+            titleStackView.addArrangedSubview(subtitleLabel)
+            
+            subtitleLabel.snp.makeConstraints { make in
+                make.horizontalEdges.equalToSuperview()
+                make.height.greaterThanOrEqualTo(21)
+            }
         }
     }
     
