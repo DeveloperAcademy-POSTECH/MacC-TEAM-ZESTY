6902d3259af2585eef123581c40ffd57c12a2958
Chanhee Jeong <chan420420@gmail.com>
2022-10-24 05:13:55 +0900

    Feat ✨: Category Selection UI 구현

---
+++ Projects/App/Sources/UI/Place/AddPlace/View/Cell/CategoryIconCell.swift
@@ -0,0 +1,83 @@
+//
+//  CategoryIconCell.swift
+//  App
+//
+//  Created by Chanhee Jeong on 2022/10/24.
+//  Copyright © 2022 zesty. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import DesignSystem
+
+final class CategoryIconCell: UICollectionViewCell {
+    
+    var viewModel: Category?
+    
+    private let cellView = UIView()
+    
+    private let imageView: UIImageView = {
+        $0.contentMode = .scaleAspectFit
+        return $0
+    }(UIImageView())
+    
+    private let nameLabel: UILabel = {
+        $0.textColor = .black
+        $0.textAlignment = .center
+        $0.numberOfLines = 2
+        $0.font = .systemFont(ofSize: 14, weight: .medium)
+        return $0
+    }(UILabel())
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                UIView.animate(withDuration: 0.1) {
+                    self.contentView.backgroundColor = .black
+                    self.nameLabel.textColor = .white
+                }
+            } else {
+                UIView.animate(withDuration: 0.1) {
+                    self.contentView.backgroundColor = .white
+                    self.nameLabel.textColor = .black
+                }
+            }
+        }
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.addSubviews([imageView, nameLabel])
+            
+        imageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(UIScreen.main.isWiderThan425pt ? 25 : 15 )
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(UIScreen.main.isLessThan376pt ? 50 : 65)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+        
+        nameLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(10)
+            $0.centerX.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+    }
+    
+    func configure(with viewModel: Category) {
+        self.viewModel = viewModel
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+        self.nameLabel.text = viewModel.name
+        self.imageView.kf.setImage(with: URL(string: viewModel.imageURL ?? "https://user-images.githubusercontent.com/63157395/197410857-e13c1bbb-b19a-4c59-a493-77501a4a529b.png"))
+    }
+
+}
+
+public extension UIScreen {
+  /// - Mini, SE: 375.0
+  /// - pro: 390.0
+  /// - pro max: 428.0
+  var isWiderThan425pt: Bool { self.bounds.size.width > 425 }
+  var isLessThan376pt: Bool { self.bounds.size.width < 376 }
+}
