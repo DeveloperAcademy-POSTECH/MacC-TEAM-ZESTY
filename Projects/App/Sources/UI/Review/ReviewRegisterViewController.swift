0ff2d640dc820bb81e9b941ef084972cad041d72
Lia316 <kjk0825@postech.ac.kr>
2022-10-23 23:12:14 +0900

    Refactor♻️: hide view animation 코드 extension 파일로 분리

--- Projects/App/Sources/UI/Review/ReviewRegisterViewController.swift
+++ Projects/App/Sources/UI/Review/ReviewRegisterViewController.swift
@@ -221,25 +221,3 @@ struct ReviewRegisterPreview: PreviewProvider {
     }
     
 }
-
-extension UIView {
-    
-    func willHide(with duration: CGFloat) {
-        UIView.animateKeyframes(withDuration: duration, delay: 0) {
-            self.alpha = 0
-        } completion: { _ in
-            self.alpha = 1
-            self.isHidden = true
-        }
-    }
-
-    func willShow(with duration: CGFloat) {
-        self.isHidden = false
-        self.alpha = 0
-        UIView.animateKeyframes(withDuration: duration, delay: 0) {
-            self.alpha = 1
-        } completion: { _ in
-        }
-    }
-    
-}
