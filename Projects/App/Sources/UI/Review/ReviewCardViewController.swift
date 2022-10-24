1dc60255c089161e302bd030917f32d83dfe76a9
Lia316 <kjk0825@postech.ac.kr>
2022-10-24 17:22:54 +0900

    Design🎨: review card VC UI

--- Projects/App/Sources/UI/Review/ReviewCardViewController.swift
+++ Projects/App/Sources/UI/Review/ReviewCardViewController.swift
@@ -9,6 +9,7 @@
 import Combine
 import SwiftUI
 import UIKit
+import DesignSystem
 import SnapKit
 
 final class ReviewCardViewController: UIViewController {
@@ -16,7 +17,11 @@ final class ReviewCardViewController: UIViewController {
     // MARK: - Properties
     private let cancelBag = Set<AnyCancellable>()
     
+    private let safeArea = UIView()
+    private let titleView = MainTitleView(title: "리뷰 등록 완료 🎉")
     private let cardView = ReviewCardView()
+    private let saveButton = UIButton()
+    private let completeButton = FullWidthBlackButton()
     
     // MARK: - LifeCycle
     
@@ -27,6 +32,11 @@ final class ReviewCardViewController: UIViewController {
     }
     
     // MARK: - Function
+    @objc func saveButtonTouched() {
+    }
+    
+    @objc func completeButtonTouched() {
+    }
     
 }
 
@@ -35,14 +45,47 @@ final class ReviewCardViewController: UIViewController {
 extension ReviewCardViewController {
     
     private func configureUI() {
-        view.addSubview(cardView)
+        var config = UIImage.SymbolConfiguration(paletteColors: [.red])
+        let downloadImage = UIImage(systemName: "square.and.arrow.down", withConfiguration: config)
+        saveButton.setImage(downloadImage, for: .normal)
+        saveButton.setTitle(" 리뷰 카드 저장하기", for: .normal)
+        saveButton.setTitleColor(.red, for: .normal)
+        saveButton.titleLabel?.font = .preferredFont(forTextStyle: .footnote).bold()
+        saveButton.addTarget(self, action: #selector(saveButtonTouched), for: .touchUpInside)
+        
+        completeButton.setTitle("완료", for: .normal)
+        completeButton.addTarget(self, action: #selector(completeButtonTouched), for: .touchUpInside)
     }
     
     private func createLayout() {
+        safeArea.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(safeArea)
+        
+        let guide = view.safeAreaLayoutGuide
+        safeArea.topAnchor.constraint(equalTo: guide.topAnchor).isActive = true
+        safeArea.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+        safeArea.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+        safeArea.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+        
+        view.addSubviews([titleView, cardView, saveButton, completeButton])
+        
+        titleView.snp.makeConstraints {
+            $0.top.equalTo(safeArea.snp.top)
+            $0.horizontalEdges.equalToSuperview()
+        }
         cardView.snp.makeConstraints {
             $0.center.equalToSuperview()
             $0.horizontalEdges.equalToSuperview().inset(45)
             $0.height.equalTo(cardView.snp.width).multipliedBy(1.3)
+        }
+        saveButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(cardView.snp.bottom).offset(30)
+            $0.width.equalTo(120)
+            $0.height.equalTo(20)
+        }
+        completeButton.snp.makeConstraints {
+            $0.horizontalEdges.bottom.equalTo(safeArea).inset(20)
         }
     }
     
@@ -53,7 +96,7 @@ extension ReviewCardViewController {
 struct ReviewCardVCPreview: PreviewProvider {
     
     static var previews: some View {
-        ReviewCardViewController().toPreview()
+        UINavigationController(rootViewController: ReviewCardViewController()).toPreview()
     }
     
 }
