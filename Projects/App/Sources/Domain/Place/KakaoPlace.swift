e27e121466308bcc8143f874908a22078e53cb4a
Chanhee Jeong <chan420420@gmail.com>
2022-10-26 03:50:29 +0900

    Feat✨: 카카오Place 및 카카오PlaceId 추가

---
+++ Projects/App/Sources/Domain/Place/KakaoPlace.swift
@@ -0,0 +1,17 @@
+//
+//  KakaoPlace.swift
+//  App
+//
+//  Created by Chanhee Jeong on 2022/10/26.
+//  Copyright © 2022 zesty. All rights reserved.
+//
+
+import Foundation
+
+struct KakaoPlace {
+    let id: Int
+    let placeName: String
+    let latitude: String
+    let longitude: String
+    let roadAddress: String
+}
