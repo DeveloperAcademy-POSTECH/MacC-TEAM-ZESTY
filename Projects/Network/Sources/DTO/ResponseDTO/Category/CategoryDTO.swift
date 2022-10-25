c84f56158af18c6eeca96ebececfd4d25b0a6f75
Chanhee Jeong <chan420420@gmail.com>
2022-10-26 06:32:37 +0900

    Feat✨: 카테고리 데이터레이어 구성

---
+++ Projects/Network/Sources/DTO/ResponseDTO/Category/CategoryDTO.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryDTO.swift
+//  Network
+//
+//  Created by Chanhee Jeong on 2022/10/26.
+//  Copyright © 2022 zesty. All rights reserved.
+//
+
+import Foundation
+
+public struct CategoryDTO: Codable {
+    public let id: Int
+    public let name: String
+    public let img: String
+    public let createdAt, updatedAt: String
+}
+
+public typealias CategoryListDTO = [CategoryDTO]
