44e9f26772bdd4a0dccbc0328b038e66ee45d6a1
italsehf <alsehfdl0316@naver.com>
2022-10-26 14:26:26 +0900

    Refactor:recycle:: 오토레이아웃 깨지는 현상 수정

--- Projects/App/Sources/UI/User/Profile/ProfileViewController.swift
+++ Projects/App/Sources/UI/User/Profile/ProfileViewController.swift
@@ -14,6 +14,10 @@ final class ProfileViewController: UIViewController {
     
     // MARK: - Properties
     private let profileImage = UIImageView()
+    private let instaButton = UIButton()
+    private let mailButton = UIButton()
+    private let instaLink = UILabel()
+    private let mailLink = UILabel()
     private var profileNickNameView = ProfileNickNameView()
 
     // MARK: - LifeCycle
@@ -38,20 +42,59 @@ extension ProfileViewController {
         profileImage.image = UIImage(.img_signup)
         profileImage.contentMode = .scaleAspectFit
         
+        instaButton.setImage(UIImage(.btn_insta), for: .normal)
+        
+        instaLink.text = "인스타"
+        instaLink.textColor = .black
+        instaLink.font = .systemFont(ofSize: 13, weight: .regular)
+        instaLink.textAlignment = .center
+        instaLink.numberOfLines = 1
+
+        mailButton.setImage(UIImage(.btn_mail), for: .normal)
+
+        mailLink.text = "문의"
+        mailLink.textColor = .black
+        mailLink.font = .systemFont(ofSize: 13, weight: .regular)
+        mailLink.textAlignment = .center
+        mailLink.numberOfLines = 1
+
         profileNickNameView = ProfileNickNameView()
         
     }
     
     private func createLayout() {
-        view.addSubview(profileImage)
-        view.addSubview(profileNickNameView)
+        view.addSubviews([profileImage, profileNickNameView, instaButton, instaLink, mailButton, mailLink])
         
         profileImage.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.horizontalEdges.top.equalTo(view.safeAreaLayoutGuide).inset(30)
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(30)
             make.height.equalTo(100)
         }
 
+        instaLink.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(30)
+            make.centerX.equalTo(instaButton.snp.centerX)
+        }
+        
+        instaButton.snp.makeConstraints { make in
+            make.bottom.equalTo(instaLink.snp.top).offset(-10)
+            make.leading.equalToSuperview().inset(115)
+            make.trailing.equalToSuperview().inset(215)
+            make.width.equalTo(60)
+        }
+        
+        mailLink.snp.makeConstraints { make in
+            make.bottom.bottom.equalTo(view.safeAreaLayoutGuide).inset(30)
+            make.centerX.equalTo(mailButton.snp.centerX)
+        }
+        
+        mailButton.snp.makeConstraints { make in
+            make.bottom.equalTo(instaLink.snp.top).offset(-10)
+            make.trailing.equalToSuperview().inset(115)
+            make.leading.equalToSuperview().inset(215)
+            make.width.equalTo(60)
+        }
+        
         profileNickNameView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(profileImage.snp.bottom).offset(20)
