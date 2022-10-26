7b67f04c7445f425b8dd57cc2c5ea6ee2043d3f9
italsehf <alsehfdl0316@naver.com>
2022-10-26 20:48:04 +0900

    Chore:broom:: SceneDelegate 원상복귀

--- Projects/App/Sources/Application/SceneDelegate.swift
+++ Projects/App/Sources/Application/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: ProfileViewController())
+        let navigationController = UINavigationController(rootViewController: ViewController())
         window.rootViewController = navigationController
         self.window = window
         window.makeKeyAndVisible()
