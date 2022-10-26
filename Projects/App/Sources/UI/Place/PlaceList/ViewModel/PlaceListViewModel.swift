713d1545fc8154242cb97843001de8157415fa6c
Lia316 <kjk0825@postech.ac.kr>
2022-10-26 17:10:57 +0900

    Feat✨: ViewModel Input & Output 정의

---
+++ Projects/App/Sources/UI/Place/PlaceList/ViewModel/PlaceListViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  PlaceListViewModel.swift
+//  App
+//
+//  Created by 리아 on 2022/10/26.
+//  Copyright © 2022 zesty. All rights reserved.
+//
+
+import Combine
+import Foundation
+
+class PlaceListViewModel {
+    
+    // MARK: - Properties
+    
+    private let useCase: PlaceListUseCase
+    private var cancelBag = Set<AnyCancellable>()
+    
+    // Input
+    @Published var cursor: Int = 1
+    
+    // Output
+    struct Result {
+        struct Review {
+            var image: String?
+            var menu: String?
+        }
+        
+        var placeName: String
+        var evaluationSum: EvaluationSum
+        var review: Review
+        
+        init(placeName: String = "",
+             evaluationSum: EvaluationSum = EvaluationSum(good: 0, soso: 0, bad: 0),
+             review: Review = Review()
+             ) {
+            self.placeName = placeName
+            self.evaluationSum = evaluationSum
+            self.review = review
+        }
+    }
+    
+    @Published var result = Result()
+    let isRegisterFail = PassthroughSubject<String, Never>() // alert 용
+    
+    // MARK: - LifeCycle
+    
+    init(useCase: PlaceListUseCase = PlaceListUseCase()) {
+        self.useCase = useCase
+    }
+    
+}
+
+// MARK: - Functions
+
+extension PlaceListViewModel {
+    
+}
