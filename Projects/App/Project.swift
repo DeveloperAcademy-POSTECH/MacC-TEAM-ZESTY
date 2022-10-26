ac7224616accf5bd2ad98e6099d222967fd24c50
Goban <gobanest@gmail.com>
2022-10-26 15:44:34 +0900

    Chore:broom:: 오타 수정

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -16,7 +16,7 @@ let project = Project.makeAppModule(
         .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.0.1")),
         .remote(url: "https://github.com/onevcat/Kingfisher.git", requirement: .upToNextMajor(from: "7.0.0")),
         .remote(url: "https://github.com/kakao/kakao-ios-sdk", requirement: .upToNextMajor(from: "2.11.3")),
-        .remote(url: "https://github.com/awslabs/aws-sdk-swift", requirement: .upToNextMajor(from: "0.3.1"))
+        .remote(url: "https://github.com/awslabs/aws-sdk-swift", requirement: .upToNextMajor(from: "0.3.1")),
         .remote(url: "https://github.com/firebase/firebase-ios-sdk", requirement: .upToNextMajor(from: "9.0.0"))
     ],
     dependencies: [
