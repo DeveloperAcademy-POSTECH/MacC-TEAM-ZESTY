6d1e94c36187b0a7d60370f4cf3df458a966a88a
italsehf <alsehfdl0316@naver.com>
2022-10-28 00:35:26 +0900

    Chore:broom:: conflict 사항 수정

--- Projects/App/Sources/Global/Extension/UIScreen+.swift
+++ Projects/App/Sources/Global/Extension/UIScreen+.swift
@@ -12,6 +12,7 @@ public extension UIScreen {
   /// - Mini, SE: 375.0
   /// - pro: 390.0
   /// - pro max: 428.0
-  var isWiderThan425pt: Bool { self.bounds.size.width > 425 }
+  var isWiderThan425pt: Bool { self.bounds.size.width > 425 } // MAX
+  var isLessThan376pt: Bool { self.bounds.size.width < 376 } // MINI
   var isHeightLessThan670pt: Bool { self.bounds.size.height < 670 }
 }
