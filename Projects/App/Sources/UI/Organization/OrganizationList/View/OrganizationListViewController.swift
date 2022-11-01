43a66de236b7802b8263129a3f86a05343c11056
Nick(taehoKim) <xogh8025@gmail.com>
2022-11-01 12:17:20 +0900

    Revert "Feat:sparkles:: navigationBar를 수정하는 함수 추가"
    This reverts commit 85b749c5fbe67f2553b1c5480795e9ad8f75da0a.

--- Projects/App/Sources/UI/Organization/OrganizationList/View/OrganizationListViewController.swift
+++ Projects/App/Sources/UI/Organization/OrganizationList/View/OrganizationListViewController.swift
@@ -94,7 +94,9 @@ extension OrganizationListViewController {
     private func configureUI() {
         view.backgroundColor = .white
         
-        setNavigationBar()
+        title = "대학 선택"
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.topItem?.title = ""
         
         titleLabel.text = "참여할 대학교를 알려주세요"
         titleLabel.font = UIFont.boldSystemFont(ofSize: 26)
@@ -123,14 +125,5 @@ extension OrganizationListViewController {
             make.horizontalEdges.equalToSuperview()
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
         }
-    }
-    
-    private func setNavigationBar() {
-        navigationItem.title = "대학 선택"
-        self.navigationController?.navigationBar.titleTextAttributes = [
-            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .medium)
-        ]
-        navigationController?.navigationBar.tintColor = .black
-        navigationController?.navigationBar.topItem?.title = ""
     }
 }
