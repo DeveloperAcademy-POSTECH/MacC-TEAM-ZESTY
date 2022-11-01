fcffa11c0e38722e96815756239bf3f1013282c1
Lia316 <kjk0825@postech.ac.kr>
2022-11-01 12:39:35 +0900

    Refactor♻️: diffableDatasource로 변경, headerView 분리, table → collection

--- Projects/App/Sources/UI/Place/PlaceList/View/PlaceListViewController.swift
+++ Projects/App/Sources/UI/Place/PlaceList/View/PlaceListViewController.swift
@@ -1,8 +1,9 @@
 //
-//  SubPlaceListViewControllerViewController.swift
+//  PlaceListViewController.swift
 //  App
 //
 //  Created by 김태호 on 2022/10/25.
+//  Updated by 리아 on 2022/11/01.
 //  Copyright (c) 2022 zesty. All rights reserved.
 //
 
@@ -16,15 +17,14 @@ final class PlaceListViewController: UIViewController {
 
     private var cancelBag = Set<AnyCancellable>()
     private let viewModel: PlaceListViewModel
-
-    private let headerView = UIView()
-    private let segmentIndicator = UIView()
-    private let segmentedControl = UISegmentedControl(items: ["전체", "선정맛집"])
     
-    private let questionButton = UIButton()
-    private let addPlaceButton = UIButton()
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, Place>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Place>
     
-    private let tableView = UITableView()
+    private var collectionView: UICollectionView!
+    private var dataSource: DataSource!
+    private var snapshot = Snapshot()
+    private let headerType = "section-header-element-kind"
     
     // MARK: - LifeCycle
     
@@ -39,9 +39,11 @@ final class PlaceListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
-        createLayout()
-        bind()
+//        configureUI()
+//        configureHierarchy()
+//        configureDataSource()
+//        applyInitialSnapShot()
+//        bind()
     }
     
     // MARK: - Function
@@ -49,44 +51,24 @@ final class PlaceListViewController: UIViewController {
     private func bind() {
         viewModel.$result
             .receive(on: DispatchQueue.main)
-            .sink { [weak self] _ in
+            .sink { [weak self] places in
                 guard let self = self else { return }
-                self.tableView.reloadData()
+                self.snapshot.appendItems(places)
+                self.dataSource.apply(self.snapshot)
             }
             .store(in: &cancelBag)
     }
 
 }
 
-extension PlaceListViewController: UITableViewDataSource, UITableViewDataSourcePrefetching {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.result.count
-    }
+extension PlaceListViewController: UICollectionViewDataSourcePrefetching, UICollectionViewDelegate {
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: PlaceCell.identifier, for: indexPath) as? PlaceCell
-        guard let cell = cell else { return UITableViewCell() }
-        let place = viewModel.result[indexPath.row]
-        cell.setUp(with: place)
-        cell.selectionStyle = .none
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
         let rows = indexPaths.map { $0.row }
         viewModel.prefetch(at: rows)
     }
     
-}
-
-extension PlaceListViewController: UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let placeId = viewModel.result[indexPath.row].id
         let placeDetailViewModel = PlaceDetailViewModel(placeId: placeId)
         show(PlaceDetailViewController(viewModel: placeDetailViewModel), sender: nil)
@@ -97,38 +79,6 @@ extension PlaceListViewController: UITableViewDelegate {
 // MARK: - UI function
 
 extension PlaceListViewController {
-    
-    @objc func indexChanged(_ sender: UISegmentedControl) {
-        let numberOfSegments = CGFloat(segmentedControl.numberOfSegments)
-        let selectedIndex = CGFloat(sender.selectedSegmentIndex)
-        let titlecount = CGFloat((segmentedControl.titleForSegment(at: sender.selectedSegmentIndex)!.count))
-        if selectedIndex == 1 {
-            viewModel.placeType = .hot
-            segmentIndicator.snp.remakeConstraints { (make) in
-                make.top.equalTo(segmentedControl.snp.bottom).offset(3)
-                make.height.equalTo(3)
-                make.width.equalTo(titlecount * 11)
-                make.centerX.equalTo(segmentedControl.snp.centerX).dividedBy(numberOfSegments / CGFloat(2.58 + CGFloat(selectedIndex-1.0)*2.0))
-            }
-        } else {
-            viewModel.placeType = .whole
-            segmentIndicator.snp.makeConstraints { make in
-                make.top.equalTo(segmentedControl.snp.bottom).offset(3)
-                make.height.equalTo(3)
-                make.width.equalTo(20)
-                make.centerX.equalTo(segmentedControl.snp.centerX).dividedBy(segmentedControl.numberOfSegments)
-            }
-        }
-        
-        UIView.animate(withDuration: 0.3, animations: {
-            self.view.layoutIfNeeded()
-            self.segmentIndicator.transform = CGAffineTransform(scaleX: 1.1, y: 1)
-        }) { _ in
-            UIView.animate(withDuration: 0.4, animations: {
-                self.segmentIndicator.transform = CGAffineTransform.identity
-            })
-        }
-    }
     
     @objc func searchButtonTapped() {
         // TODO: 검색뷰 완성 시 연결
@@ -141,9 +91,63 @@ extension PlaceListViewController {
         // TODO: 민이 만든 프로필 뷰로 이동
          navigationController?.pushViewController(ProfileViewController(), animated: true)
     }
+
+}
+
+// MARK: - Configure CollectionView
+
+extension PlaceListViewController {
+
+    private func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.frame, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .clear
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+    }
+
+    private func createLayout() -> UICollectionViewLayout {
+        let section: NSCollectionLayoutSection
+        let margin: CGFloat = 10
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(90))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(90))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        section = NSCollectionLayoutSection(group: group)
+        
+        let sectionHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(85))
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: sectionHeaderSize,
+                                                                        elementKind: headerType,
+                                                                        alignment: .topLeading)
+        section.boundarySupplementaryItems = [sectionHeader]
+        section.contentInsets = NSDirectionalEdgeInsets(top: margin, leading: margin, bottom: margin, trailing: margin)
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
     
-    @objc func addPlaceButtonTapped() {
-        navigationController?.pushViewController(AddPlaceSearchViewController(viewModel: AddPlaceSearchViewModel()), animated: true)
+    private func configureDataSource() {
+        let cellRegisteration = UICollectionView.CellRegistration<PlaceCell, Place> { cell, _, item in
+            cell.setUp(with: item)
+        }
+        typealias SupplymentaryViewRegistration = UICollectionView.SupplementaryRegistration<SegmentHeaderView>
+        let headerRegisteration = SupplymentaryViewRegistration(elementKind: headerType) { [weak self] supplementaryView, _, _ in
+            guard let self = self else { return }
+            supplementaryView.viewModel = self.viewModel
+        }
+        
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegisteration, for: indexPath, item: item)
+        })
+        dataSource.supplementaryViewProvider = { collectionView, _, index in
+            return self.collectionView.dequeueConfiguredReusableSupplementary(using: headerRegisteration, for: index)
+        }
+    }
+
+    private func applyInitialSnapShot() {
+        snapshot.appendSections([1])
+        snapshot.appendItems([], toSection: 1)
+        dataSource.apply(snapshot)
     }
 
 }
@@ -151,40 +155,7 @@ extension PlaceListViewController {
 extension PlaceListViewController {
     
     private func configureUI() {
-        tableView.backgroundColor = .clear
-        tableView.dataSource = self
-        tableView.prefetchDataSource = self
-        tableView.delegate = self
-        tableView.register(PlaceCell.self, forCellReuseIdentifier: PlaceCell.identifier)
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 300
-        tableView.separatorStyle = .none
-
-        removeBackgroundAndDivider()
         configureNaviBar()
-        
-        view.backgroundColor = .white
-        headerView.backgroundColor = .white
-        segmentIndicator.backgroundColor = .black
-        
-        segmentedControl.backgroundColor = .clear
-        segmentedControl.tintColor = .clear
-        segmentedControl.apportionsSegmentWidthsByContent = true
-        segmentedControl.selectedSegmentIndex = 0
-        
-        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 26, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.lightGray], for: .normal)
-        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 26, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.black], for: .selected)
-        segmentedControl.addTarget(self, action: #selector(indexChanged), for: .valueChanged)
-        
-        let imageConfiguration = UIImage.SymbolConfiguration(weight: .semibold)
-        let questionMarkImage = UIImage(systemName: "questionmark.circle", withConfiguration: imageConfiguration)
-        
-        questionButton.setImage(questionMarkImage, for: .normal)
-        questionButton.tintColor = .zestyColor(.gray54)
-        
-        let sidePlusImage = UIImage(.btn_side_plus)
-        addPlaceButton.setImage(sidePlusImage, for: .normal)
-        addPlaceButton.addTarget(self, action: #selector(addPlaceButtonTapped), for: .touchUpInside)
     }
     
     private func configureNaviBar() {
@@ -201,56 +172,6 @@ extension PlaceListViewController {
         navigationItem.hidesBackButton = true
         
         navigationController?.navigationBar.tintColor = .black
-    }
-    
-    private func removeBackgroundAndDivider() {
-        let removeBackgroundDivider = UIImage()
-        segmentedControl.setBackgroundImage(removeBackgroundDivider, for: .normal, barMetrics: .default)
-        segmentedControl.setBackgroundImage(removeBackgroundDivider, for: .selected, barMetrics: .default)
-        segmentedControl.setDividerImage(removeBackgroundDivider, forLeftSegmentState: .selected, rightSegmentState: .normal, barMetrics: .default)
-    }
-    
-    private func createLayout() {
-        view.addSubviews([headerView, tableView])
-        headerView.addSubviews([segmentedControl, segmentIndicator, questionButton, addPlaceButton])
-        
-        headerView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            make.horizontalEdges.equalToSuperview()
-            make.height.equalTo(85)
-        }
-        
-        segmentedControl.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
-            make.left.equalToSuperview().inset(20)
-            make.width.equalTo(172)
-            make.height.equalTo(32)
-        }
-        
-        segmentIndicator.snp.makeConstraints { make in
-            make.top.equalTo(segmentedControl.snp.bottom).offset(3)
-            make.height.equalTo(3)
-            make.width.equalTo(20)
-            make.centerX.equalTo(segmentedControl.snp.centerX).dividedBy(segmentedControl.numberOfSegments)
-        }
-        
-        questionButton.snp.makeConstraints { make in
-            make.left.equalTo(segmentedControl.snp.right)
-            make.top.equalTo(segmentedControl.snp.top)
-            make.width.equalTo(21)
-            make.height.equalTo(21)
-        }
-        
-        addPlaceButton.snp.makeConstraints { make in
-            make.centerY.right.equalToSuperview()
-            make.width.equalTo(70)
-            make.height.equalTo(65)
-        }
-        
-        tableView.snp.makeConstraints { make in
-            make.top.equalTo(headerView.snp.bottom)
-            make.horizontalEdges.bottom.equalToSuperview()
-        }
     }
     
 }
