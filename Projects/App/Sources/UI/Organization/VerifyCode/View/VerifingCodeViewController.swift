82914e476e627d6c8c9f4b4e8e1714313fbe7132
Nick(taehoKim) <xogh8025@gmail.com>
2022-11-02 20:02:11 +0900

    Feat:sparkles:: VerifingCodeViewController UI 구현
      - MainTitleView를 추가하였습니다
      - 이전 커밋에서 MainTitleView의 변경이 있습니다 : SFsymbol의 추가를 위함
      - OTPStackView를 추가하였습니다
      - UI만 구현되어 있습니다

--- Projects/App/Sources/UI/Organization/VerifyCode/View/VerifingCodeViewController.swift
+++ Projects/App/Sources/UI/Organization/VerifyCode/View/VerifingCodeViewController.swift
@@ -8,6 +8,7 @@
 
 import Combine
 import UIKit
+import DesignSystem
 import SnapKit
 
 final class VerifingCodeViewController: UIViewController {
@@ -15,6 +16,24 @@ final class VerifingCodeViewController: UIViewController {
     // MARK: - Properties
     private let cancelBag = Set<AnyCancellable>()
     
+    private lazy var titleView = MainTitleView(title: "이메일로 받은 코드를\n알려주세요", subtitle: "\(userEmail)", hasSymbol: true)
+    
+    private let warningMessage = UILabel()
+    private let otpStackView = OTPStackView()
+    private let timerLabel = UILabel()
+    
+    private let resendStackView = UIStackView()
+    private let resendLabel = UILabel()
+    private let resendEamilButton = UIButton(type: .custom)
+    
+    private let arrowButton = ArrowButton(initialDisable: true)
+    
+    // TODO: ViewModel로 옮길 것들입니다
+    let userEmail = "mingming@pos.idserve.net"
+    var isArrowButtonHidden: Bool = true
+    var isCodeValid: Bool = true
+    var userInputCode: String = ""
+    var timer = "03:00"
     
     // MARK: - LifeCycle
     
@@ -28,29 +47,83 @@ final class VerifingCodeViewController: UIViewController {
     
 }
 
-
 // MARK: - UI Function
 
 extension VerifingCodeViewController {
     
     private func configureUI() {
+        warningMessage.text = "잘못된 코드에요."
+        warningMessage.isHidden = isCodeValid
         
+        timerLabel.text = timer
+        
+        resendStackView.spacing = 10
+        resendStackView.axis = .horizontal
+        resendStackView.distribution = .fill
+        resendStackView.alignment = .fill
+        
+        resendLabel.text = "메일이 오지 않았나요?"
+        resendLabel.font = .systemFont(ofSize: 13, weight: .regular)
+        resendLabel.numberOfLines = 0
+        
+        resendEamilButton.setTitle("다시 보내기", for: .normal)
+        resendEamilButton.setTitleColor(.label, for: .normal)
+        resendEamilButton.titleLabel?.font = .systemFont(ofSize: 13, weight: .bold)
+        
+        arrowButton.isHidden = isArrowButtonHidden
+        arrowButton.startIndicator()
     }
     
     private func createLayout() {
+        view.addSubviews([titleView, warningMessage, otpStackView, timerLabel, resendStackView, arrowButton])
+        resendStackView.addArrangedSubviews([resendLabel, resendEamilButton])
         
+        titleView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.horizontalEdges.equalToSuperview()
+        }
+        
+        warningMessage.snp.makeConstraints { make in
+            make.top.equalTo(titleView.snp.bottom).offset(82)
+            make.centerX.equalToSuperview()
+        }
+        
+        otpStackView.snp.makeConstraints { make in
+            make.top.equalTo(warningMessage.snp.bottom).offset(15)
+            make.height.equalTo(50)
+            make.width.equalTo(190)
+            make.centerX.equalToSuperview()
+        }
+        
+        timerLabel.snp.makeConstraints { make in
+            make.top.equalTo(otpStackView.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+        }
+        
+        resendStackView.snp.makeConstraints { make in
+            make.top.equalTo(timerLabel.snp.bottom).offset(43)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(50)
+        }
+        
+        arrowButton.snp.makeConstraints { make in
+            make.right.equalToSuperview().inset(20)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(20)
+        }
     }
     
 }
 
-/*
 // MARK: - Previews
 
-extension VerifingCodePreview: PreviewProvider {
+#if DEBUG
+import SwiftUI
+
+struct VerifingCodePreview: PreviewProvider {
     
     static var previews: some View {
         VerifingCodeViewController().toPreview()
     }
     
 }
-*/
+#endif
