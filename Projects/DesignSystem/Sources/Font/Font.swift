525bc9a4f313e6740beaebb8ca9ab11cf02ed395
Chaeho-Min <james118@naver.com>
2022-11-03 21:46:08 +0900

    Feat✨: Font enum 추가

---
+++ Projects/DesignSystem/Sources/Font/Font.swift
@@ -0,0 +1,59 @@
+//
+//  Font.swift
+//  DesignSystem
+//
+//  Created by 민채호 on 2022/11/03.
+//  Copyright © 2022 com.zesty. All rights reserved.
+//
+
+import UIKit
+
+public enum Font {
+    
+    public enum Size: CGFloat {
+        case largeTitle = 26
+        case title2 = 22
+        case title3 = 20
+        case body = 17
+        case callout = 16
+        case footnote = 13
+        case caption1 = 12
+        case caption2 = 11
+        case caption3 = 9
+    }
+    
+    public enum Weight {
+        case ultraLight
+        case thin
+        case light
+        case regular
+        case medium
+        case semibold
+        case bold
+        case heavy
+        case black
+        
+        var value: UIFont.Weight {
+            switch self {
+            case .ultraLight:
+                return .ultraLight
+            case .thin:
+                return .thin
+            case .light:
+                return .light
+            case .regular:
+                return .regular
+            case .medium:
+                return .medium
+            case .semibold:
+                return .semibold
+            case .bold:
+                return .bold
+            case .heavy:
+                return .heavy
+            case .black:
+                return .black
+            }
+        }
+    }
+}
--- Projects/DesignSystem/Sources/Font/SourcesFontHolder.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Font.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by Lee Myeonghwan on 2022/10/07.
-//
-
-import Foundation
