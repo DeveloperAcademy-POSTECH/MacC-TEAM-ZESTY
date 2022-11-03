e8643e7cc2ff28e8ef37f76011a513a083133d02
Chaeho-Min <james118@naver.com>
2022-11-03 21:50:37 +0900

    Feat✨: UIFont 익스텐션 추가
    - UIFont에서 Font enum을 사용할 수 있도록 하는 zestyFont 메서드 추가
    - 이전에 DesignSystem 모듈의 Extension 폴더에 만들어놓은 UIFont+ 코드를 여기로 병합

--- Projects/DesignSystem/Sources/Font/UIFont+.swift
+++ Projects/DesignSystem/Sources/Font/UIFont+.swift
@@ -2,14 +2,18 @@
 //  UIFont+.swift
 //  DesignSystem
 //
-//  Created by 리아 on 2022/10/24.
-//  Copyright © 2022 zesty. All rights reserved.
+//  Created by 민채호 on 2022/11/03.
+//  Copyright © 2022 com.zesty. All rights reserved.
 //
 
 import UIKit
 
 public extension UIFont {
+    class func zestyFont(size: Font.Size, weight: Font.Weight) -> UIFont {
+        return .systemFont(ofSize: size.rawValue, weight: weight.value)
+    }
     
+    // TODO: 폰트 시스템 적용 후 삭제 예정
     private func withTraits(traits: UIFontDescriptor.SymbolicTraits) -> UIFont {
         let descriptor = fontDescriptor.withSymbolicTraits(traits)
         return UIFont(descriptor: descriptor!, size: 0)
@@ -22,5 +26,4 @@ public extension UIFont {
     func italic() -> UIFont {
         return withTraits(traits: .traitItalic)
     }
-    
 }
