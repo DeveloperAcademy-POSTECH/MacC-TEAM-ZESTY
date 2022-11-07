2f14a4bee0ef888b191e9bfeaee3a74b6a85cb5b
Nick(taehoKim) <xogh8025@gmail.com>
2022-11-07 12:13:59 +0900

    Feat:sparkles:: DomainSettingUseCase 생성
     - 기존 UserSignupUseCase에 있던 postSingupUser를 삭제하였습니다.
     - DomainSettingUseCase를 만들어 postSingupUser를 postUserEmail로 변경하였습니다
     - API문서 그대로 postUserEmail의 경우 user api 영역에 있으므로 UserAPI에 남겨뒀습니다.
       하지만 프론트엔드에서는 domainSetting의 영역이기 때문에 따로 DomainSettingUseCase를 만들어 UserAPI에서 호출하였습니다

---
+++ Projects/App/Sources/Domain/Organization/DomainSettingUseCase.swift
@@ -0,0 +1,42 @@
+//
+//  DomainSettingUseCase.swift
+//  App
+//
+//  Created by 김태호 on 2022/11/07.
+//  Copyright © 2022 com.zesty. All rights reserved.
+//
+
+import Combine
+import Foundation
+import Network
+
+final class DomainSettingUseCase {
+    
+    // output
+    let isEmailOverlapedSubject = PassthroughSubject<Bool, Never>()
+    
+    private var cancelBag = Set<AnyCancellable>()
+    
+    func postUserEmail(email: String, orgnization: Organization) {
+//        guard let authorization = UserDefaults.standard.authToken else { return }
+        let authorization = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ6ZXN0eSIsImlhdCI6MTY2NzE4MDc5MiwiZXhwIjoxNjY4MzkwMzkyLCJzdWIiOiIzNyJ9.jZ35GteW1sM9oz_QQ0cjdxk4p0CSf81Gzv7Kjs_W-zA"
+        let userDTO = SignUpUserDTO(id: orgnization.id, email: email, organizationName: orgnization.name)
+
+        UserAPI.postSignUp(authorization: authorization, userDTO: userDTO)
+            .sink { [weak self] error in
+                guard let self = self else { return }
+                switch error {
+                case .failure(let error):
+                    print(error.localizedString)
+                    self.isEmailOverlapedSubject.send(true)
+                case .finished: break
+                }
+            } receiveValue: { [weak self] _ in
+                print()
+                guard let self = self else { return }
+                self.isEmailOverlapedSubject.send(false)
+            }
+            .store(in: &cancelBag)
+    }
+    
+}
