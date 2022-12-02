208d02bb054dcffeed32d951ab524a74483f500f
Chaeho-Min <james118@naver.com>
2022-12-03 01:16:27 +0900

    Chore🧹: 레거시 색상 제거

--- Projects/DesignSystem/Sources/Color/Color.swift
+++ Projects/DesignSystem/Sources/Color/Color.swift
@@ -33,14 +33,4 @@ public enum Color: String {
     case bluegray04
     
     case red01
-    
-    // TODO: 뷰에 색상 적용 후 레거시 컬러 제거 예정
-    case background
-    case disabled
-    case dim
-    case point
-    case grayC5
-    case gray3C
-    case gray3C3C43
-    case whiteEBEBF5
 }
